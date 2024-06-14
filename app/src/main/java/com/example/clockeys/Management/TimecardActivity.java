0db5f0158892be7115316314f8424958a9048e6d
ethanman102 <ethankey.s@yahoo.com>
2024-06-13 23:08:55 -0600

    commits

--- app/src/main/java/com/example/clockeys/Management/TimecardActivity.java
+++ app/src/main/java/com/example/clockeys/Management/TimecardActivity.java
@@ -1,16 +1,55 @@
 package com.example.clockeys.Management;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.clockeys.R;
 
 public class TimecardActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timecard);
+
+        toolbar = findViewById(R.id.timecardToolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_timecard_menu,menu);
+       return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.dateRangeTimecard:
+                // Handle the settings click here
+                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            // Add more cases for other menu items if needed
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }

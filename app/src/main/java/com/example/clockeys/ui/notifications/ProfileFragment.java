bed9fffbf969df75d88f205e4d5389508f9e87c8
ethanman102 <ethankey.s@yahoo.com>
2024-06-26 23:21:37 -0600

    Update ProfileFragment.java

--- app/src/main/java/com/example/clockeys/ui/notifications/ProfileFragment.java
+++ app/src/main/java/com/example/clockeys/ui/notifications/ProfileFragment.java
@@ -59,7 +59,7 @@ public class ProfileFragment extends Fragment {
         infoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                startInfoActivity();
             }
         });
 

77c7e473c328e6ad4d4b603c0f19c44ad1c3c7ac
ethanman102 <ethankey.s@yahoo.com>
2024-06-27 01:20:00 -0600

    creating activities

---
+++ app/src/main/java/com/example/clockeys/Management/ProfileInputActivity.java
@@ -0,0 +1,16 @@
+package com.example.clockeys.Management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.clockeys.R;
+
+public class ProfileInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_input);
+    }
+}

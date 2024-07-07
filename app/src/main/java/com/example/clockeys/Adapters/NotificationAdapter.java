dd858b4f54da76e5f2143be36ba2554009945c8f
ethanman102 <ethankey.s@yahoo.com>
2024-07-07 15:04:32 -0600

    created the recycler adapter for the dynamic notifications!

---
+++ app/src/main/java/com/example/clockeys/Adapters/NotificationAdapter.java
@@ -0,0 +1,6 @@
+package com.example.clockeys.Adapters;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.MyViewHolder>{
+}

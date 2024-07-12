dbd6ddf536e3c8541fc04486e04c49e916c064c3
ethanman102 <ethankey.s@yahoo.com>
2024-07-12 00:31:15 -0600

    creating way to get to notifications

--- app/src/main/java/com/example/clockeys/MainActivity.java
+++ app/src/main/java/com/example/clockeys/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.clockeys;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+
+import com.example.clockeys.Management.ChooseNotificationActivity;
+import com.example.clockeys.Notifications.Notification;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,6 +23,7 @@ import com.example.clockeys.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +31,33 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        toolbar = findViewById(R.id.mainActivityToolbar);
+        setSupportActionBar(toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        int id = item.getItemId();
+        if (id == R.id.mainNotificationButton){
+            Intent intent = new Intent(this, Notification.class);
+            startActivity(intent);
+            return Boolean.TRUE;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }

5541fc0d09d28778b13420e7a377c7a586291f2a
ethanman102 <ethankey.s@yahoo.com>
2024-07-13 17:23:35 -0600

    capturing images

--- app/src/main/java/com/example/clockeys/Management/CameraActivity.java
+++ app/src/main/java/com/example/clockeys/Management/CameraActivity.java
@@ -1,28 +1,44 @@
 package com.example.clockeys.Management;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.OptIn;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ExperimentalGetImage;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.clockeys.R;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.nio.ByteBuffer;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 
 public class CameraActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE_PERMISSIONS = 10;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.CAMERA"};
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private ImageButton galleryButton,pictureButton,backButton;
     private PreviewView previewView;
+    private ImageCapture imageCapture;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,8 +49,23 @@ public class CameraActivity extends AppCompatActivity {
         pictureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                imageCapture.takePicture(getExecutor(), new ImageCapture.OnImageCapturedCallback() {
+                    @Override
+                    public void onCaptureSuccess(@NonNull ImageProxy image) {
+                        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+                        byte[] bytes = new byte[buffer.capacity()];
+                        buffer.get(bytes);
+                        Bitmap bitmapImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+                        imageView.setImageBitmap(bitmapImage);
+                    }
 
+                    @Override
+                    public void onError(@NonNull ImageCaptureException exception) {
+                        super.onError(exception);
+                    }
+                });
             }
+
         });
 
         cameraProviderFuture= ProcessCameraProvider.getInstance(this);
@@ -46,8 +77,9 @@ public class CameraActivity extends AppCompatActivity {
                 throw new RuntimeException(e);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            } 
+            }
         },getExecutor());
+
     }
 
     private Executor getExecutor() {
@@ -57,10 +89,14 @@ public class CameraActivity extends AppCompatActivity {
     private void startCameraX(ProcessCameraProvider cameraProvider) {
         cameraProvider.unbindAll();
 
-        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_FRONT).build();
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
 
         Preview preview = new Preview.Builder().build();
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        imageCapture = new ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY).build();
+
+        cameraProvider.bindToLifecycle(this,cameraSelector,preview,imageCapture);
     }
 
     public void bindViews(){
@@ -68,5 +104,6 @@ public class CameraActivity extends AppCompatActivity {
         pictureButton = findViewById(R.id.pictureButton);
         backButton = findViewById(R.id.cameraBackButton);
         previewView = findViewById(R.id.cameraActivityPreviewView);
+        imageView = findViewById(R.id.imageViewCamera);
     }
 }

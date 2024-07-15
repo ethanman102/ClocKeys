d55eee605f55114de9215643a759d3052f21c196
ethanman102 <ethankey.s@yahoo.com>
2024-07-15 00:49:44 -0600

    created adapter

---
+++ app/src/main/java/com/example/clockeys/Adapters/ImageAdapter.java
@@ -0,0 +1,72 @@
+package com.example.clockeys.Adapters;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.clockeys.R;
+import com.example.clockeys.Time.Punch;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<Bitmap> images;
+    private OnItemClickListener listener;
+
+    public ImageAdapter(Context context, ArrayList<Bitmap> images){
+        super();
+        this.context = context;
+        this.images = new ArrayList<>(images);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.image_view_pager_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Bitmap bitmap = images.get(position);
+        holder.image.setImageBitmap(bitmap);
+        holder.image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClick(holder.image,bitmap);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.images.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView image;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.imageViewMain);
+        }
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.listener = listener;
+    }
+    public interface OnItemClickListener{
+        void onClick(ImageView imageView,Bitmap bitmap);
+    }
+}

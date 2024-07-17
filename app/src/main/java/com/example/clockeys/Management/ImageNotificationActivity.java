63d1785f3e7468e2e66d8432c9b8c5f5d9c2869c
ethanman102 <ethankey.s@yahoo.com>
2024-07-16 22:46:49 -0600

    commit

--- app/src/main/java/com/example/clockeys/Management/ImageNotificationActivity.java
+++ app/src/main/java/com/example/clockeys/Management/ImageNotificationActivity.java
@@ -1,16 +1,77 @@
 package com.example.clockeys.Management;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.example.clockeys.Adapters.ImageAdapter;
 import com.example.clockeys.R;
+import com.example.clockeys.Users.Employee;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
 
 public class ImageNotificationActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ImageButton dismissDateButton, takePhotoButton;
+    private Button createButton;
+    private TextView dismissDateTextView;
+    private RecyclerView imageRecyclerView;
+    private TextInputEditText titleEditText, textEditText;
+    private ArrayList<Bitmap> images;
+    private ImageAdapter imageAdapter;
+    private ActivityResultLauncher<Intent> imageNotificationActivityResultLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_notification);
+
+        bindViews();
+
+        images = new ArrayList<>();
+        imageAdapter = new ImageAdapter(this,images);
+        imageRecyclerView.setAdapter(imageAdapter);
+
+        imageNotificationActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result ->{
+
+            if (result.getResultCode() == RESULT_OK && result.getData() != null){
+                Intent intent = result.getData();
+                Bitmap bitmap = intent.getParcelableExtra("image",Bitmap.class);
+                images.add(bitmap);
+                imageAdapter.notifyDataSetChanged();
+            }
+
+        });
+
     }
+
+    public void bindViews(){
+        toolbar = findViewById(R.id.imageNotificationToolbar);
+        setSupportActionBar(toolbar);
+
+        dismissDateButton = findViewById(R.id.imageDismissButton);
+        takePhotoButton = findViewById(R.id.imageNotificationTakePhotoButton);
+
+        createButton = findViewById(R.id.imageCreateButton);
+
+        dismissDateTextView = findViewById(R.id.imageDismissDate);
+
+        imageRecyclerView = findViewById(R.id.imageNotificationRecyclerView);
+
+        titleEditText = findViewById(R.id.imageTitleEditText);
+        textEditText = findViewById(R.id.imageTextEditText);
+    }
+
+
 }

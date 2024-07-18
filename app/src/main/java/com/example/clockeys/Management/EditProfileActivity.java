6803515c340c49e0edb609ca6a95626396b72e3d
ethanman102 <ethankey.s@yahoo.com>
2024-07-17 22:08:26 -0600

    finished showing the correct dialog

--- app/src/main/java/com/example/clockeys/Management/EditProfileActivity.java
+++ app/src/main/java/com/example/clockeys/Management/EditProfileActivity.java
@@ -10,12 +10,16 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -262,6 +266,12 @@ public class EditProfileActivity extends AppCompatActivity {
                 cameraActivityResultLauncher.launch(cameraIntent);
             }
         });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
 
 
     }

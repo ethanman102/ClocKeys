48359c8ff6dccef6b366b13ff287cd47f6bea127
ethanman102 <ethankey.s@yahoo.com>
2024-07-21 23:46:25 -0600

    user signup photo layout

---
+++ app/src/main/java/com/example/clockeys/Startups/UserSignup/SignUpPhotoActivity.java
@@ -0,0 +1,16 @@
+package com.example.clockeys.Startups.UserSignup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.clockeys.R;
+
+public class SignUpPhotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_photo);
+    }
+}

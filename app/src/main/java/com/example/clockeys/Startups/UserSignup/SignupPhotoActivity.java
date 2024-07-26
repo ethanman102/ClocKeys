9172f679546c65654ac3c93c6ef0257dc1dc3b97
ethanman102 <ethankey.s@yahoo.com>
2024-07-25 23:12:44 -0600

    layout changes

--- app/src/main/java/com/example/clockeys/Startups/UserSignup/SignupPhotoActivity.java
+++ app/src/main/java/com/example/clockeys/Startups/UserSignup/SignupPhotoActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.clockeys.Management.CameraActivity;
 import com.example.clockeys.R;
@@ -22,11 +23,13 @@ import com.example.clockeys.Users.Employee;
 
 import java.io.FileInputStream;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class SignupPhotoActivity extends AppCompatActivity {
 
-    private ImageButton photoButton;
+    private TextView photoButton;
     private Button confirm,remove;
-    private ImageView photo;
+    private CircleImageView photo;
     private Toolbar toolbar;
     private Intent intent;
     private Employee employee;
